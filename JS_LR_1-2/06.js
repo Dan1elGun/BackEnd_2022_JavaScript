879b554d5f62219ac640e9fa4aaf31901f87c9cb
Dan1elGun <91561829+Dan1elGun@users.noreply.github.com>
2022-09-13 16:01:47 +0300

    Add files via upload

---
+++ JS_LR_1-2/06.js
@@ -0,0 +1,32 @@
+/** ЗАДАЧА 6 - Типы данных
+ *
+ * 1. Объявите несколько переменных и присвойте им значения:
+ *  - строка
+ *  - число
+ *  - логическое
+ *  - null
+ *  - undefined
+ *  - объект
+ *  - массив
+ *
+ * 2. Выведите в консоль тип каждого из значений
+ * используя оператор typeof
+ */
+
+
+
+let str = "String";
+let num = 10;
+let bul = true;
+let nul = null;
+let und = undefined;
+let obj = {object: "object"}
+let arr = [1, 2, 3];
+
+console.log(typeof(str));
+console.log(typeof(num));
+console.log(typeof(bul));
+console.log(typeof(nul));
+console.log(typeof(und));
+console.log(typeof(obj));
+console.log(typeof(arr));
---
+++ JS_LR_1-2/10.js
@@ -0,0 +1,21 @@
+/** ЗАДАЧА 10 - Let
+ *
+ * Измените код так, чтобы ошибка исчезла
+ */
+
+myFavoriteAnimal = 'Monkey' // const myFavoriteAnimal = 'Monkey'
+
+console.log(myFavoriteAnimal)
+    // 'Monkey'
+
+myFavoriteAnimal = 'Cat' // const myFavoriteAnimal = 'Cat'
+
+console.log(myFavoriteAnimal)
+    // 'Cat'
+
+/*
+    Что изменил? 
+    Заменил const на let в 6й строке, и убрал const в 11й. 
+    Альтернативно, можно бы было просто изменить имена переменных, например, заменить следующее объявление const myFavoriteAnimal = 'Cat' на mySecondFavoriteAnimal = 'Cat', и вывести в консоль.
+    В любом случае требуется изменить 2 строки кода.
+*/    
