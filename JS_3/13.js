89526cdc70fb49ae892d4664159c52b1544df7c6
Dan1elGun <91561829+Dan1elGun@users.noreply.github.com>
2022-09-27 14:53:12 +0300

    Add files via upload

---
+++ JS_3/13.js
@@ -0,0 +1,19 @@
+/** ЗАДАЧА 13 - Создание объекта
+ *
+ * 1. Создайте объект с тремя свойствами:
+ *  - name
+ *  - surname
+ *  - favoriteNumber
+ *
+ * 2. Выведите в консоль строку
+ * "My name is <name> <surname> and my favorite number is <favoriteNumber>"
+ */
+
+
+let obj = {
+    name: "Danila",
+    surname: "Gunov",
+    favouriteNumber: 21
+}
+
+console.log(`My name is ${obj.name} ${obj.surname} and my favourite number is ${obj.favouriteNumber}`)
---
+++ JS_3/16.js
@@ -0,0 +1,17 @@
+/** ЗАДАЧА 16 - Замена части строки
+ *
+ * 1. Создайте переменную и присвойте ей строку "Good Morning"
+ *
+ * 2. Замените "Morning" на "Evening" и присвойте результат новой переменной
+ *
+ * 3. Выведите значение второй переменной в консоль
+ */
+
+let greetings = "Good Morning";
+
+let evening = "Evening";
+
+console.log(greetings.replace("Morning", "Evening"));
+
+
+
